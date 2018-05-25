d69a4c3157b31fc4bc790b2382baf0e1858de11b
papachanguito <carlos.ahuactzin@alumno.buap.mx>
2018-05-25 09:49:23 -0500

    Initial commit

---
+++ Assets/GoapAction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GoapAction : MonoBehaviour {
+
+    //una accion puede tener un costo para realizarse
+    //dependiendo del costo sera la secuencia que tome el planeador
+    public float cost;
+    
+    //precondiciones 
+    private Dictionary<string, object> Precondiciones;
+    
+    //efectos
+    private Dictionary<string, object> Efectos;
+	
+    //una accion por lo general se ejecuta sobre un objeto
+    public GameObject Target;
+
+    public GoapAction()
+    {
+        Precondiciones = new Dictionary<string, object>();
+        Efectos = new Dictionary<string, object>();
+    }
+
+    //limpiar las variables de la accion
+    private void Reset()
+    {
+        Target = null;
+    }
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/IGOAP.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*cual quier agente que ahiera usar goap tiene que implementar 
+     * esta interfaz
+     * ayudara al planeador de acciones para decidir que acciones tomar.
+     * lo uaremos tambien para ocmunicarnos ocn el agente y 
+     * hacerle saber si  yuna accion falla o se cumple*/
+
+	// Use this for initialization
+
+public interface IGOAP {
+    //crear una informacion del estado del juego o del mundo
+
+   // HashSet<KeyValuePair<string, object>> getworldState();
+
+    Dictionary<string, object> GetWorldState();
+    //promoprcionar al planeador una meta para que pueda construir la secuencia de acciones a seguir, el plan
+
+    Dictionary<string, object> CreateGoalState();
+    //
+    void PlanFound(Dictionary<string,object>Goal,
+                    Queue<GoapAction>Actions);
+
+    // si el plan falla, podriamos tratar de encotnrar uno nuevoj
+    void PlanFailed(Dictionary<string, object> FailedGoal);
+
+    void ActionsFinished();
+
+    void PlanAborted(GoapAction abortedAction);
+
+    bool MoveAgent(GoapAction nextAction);
+	
+	
+}
